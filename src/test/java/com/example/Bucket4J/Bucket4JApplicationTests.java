0a6348101583028c62f695f0ccd01cea096a328f
Rajeev Kumar <r.iitkgp@gmail.com>
2022-02-05 11:52:19 +0530

    First commit, empty project

---
+++ src/test/java/com/example/Bucket4J/Bucket4JApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Bucket4J;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bucket4JApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
